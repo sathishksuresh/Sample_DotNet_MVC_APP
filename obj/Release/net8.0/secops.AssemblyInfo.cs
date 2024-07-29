a889ff8c16c173c6a36b31e43e1a247c87263459
sathishksuresh <sathish.suresh@bootlabstech.com>
2024-07-29 16:04:12 +0530

    publish

--- obj/Release/net8.0/secops.AssemblyInfo.cs
+++ obj/Release/net8.0/secops.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("secops")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c73f54327dedcb8d2de6f8aaed6fde210c5ed5")]
 [assembly: System.Reflection.AssemblyProductAttribute("secops")]
